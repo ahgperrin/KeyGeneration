58b75cbb27c5dcd26d1e62f6366168c4210d3f37
ahgperrin <antoineperrin.pro2@gmail.com>
2021-04-18 22:39:30 +0200

    Initial Commit

---
+++ KeyGenerator/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  KeyGenerator
+//
+//  Created by Antoine Perrin on 4/18/21.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
