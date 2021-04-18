be927e8ac36525c3f34a3e06a3e32256cf6fcc29
ahgperrin <antoineperrin.pro2@gmail.com>
2021-04-18 23:11:03 +0200

    Initial Commit

--- KeyGenerator/KeyGeneration.cpp
+++ KeyGenerator/KeyGeneration.cpp
@@ -8,6 +8,7 @@
 #include <stdio.h>
 #include <iostream>
 #include "uECC.h"
+#include <pybind11/pybind11.h>
 
 using namespace std;
 
@@ -30,11 +31,10 @@ class Key
     {
         PrivateKey = key;
         cout << "Calculating public key with secp256k1 algorithm"<<endl;
-        const uint8_t* castIntPrivateKey=reinterpret_cast<const uint8_t*>(PrivateKey.c_str());
-        const int publicKeySize=uECC_curve_public_key_size(uECC_secp256k1());
-        uint8_t *varIntPublicKey = new uint8_t(publicKeySize);
-        uECC_compute_public_key(castIntPrivateKey,varIntPublicKey,uECC_secp256k1());
-        PublicKey=std::string( varIntPublicKey, varIntPublicKey+publicKeySize );
+        const int curve = uECC_curve_public_key_size(uECC_secp256k1());
+        uint8_t *modPublicKey = new uint8_t(curve);
+        PublicKey=uECC_compute_public_key(modPublicKey,modPublicKey,uECC_secp256k1());
+        PublicKey=string( modPublicKey, modPublicKey+curve );
     }
     string getPrivateKey()
     {
@@ -53,16 +53,8 @@ PYBIND11_MODULE(KeyGeneration, hab)
     hab.doc() = "KeyGeneration 1.0";
     hab.def("getVersion", &getVersion, "Version Getter");
     py::class_<KeyGeneration>(hab, "GenerateKey")
-        .def(py::init())
-        .def("initialize", &KeyGeneration::initialize)
-        .def("getPrivateKey", &KeyGeneration::getPrivateKey)
-        .def("getPublicKey", &KeyGeneration::getPublicKey);
+       .def(py::init())
+       .def("initialize", &KeyGeneration::initialize)
+       .def("getPrivateKey", &KeyGeneration::getPrivateKey)
+       .def("getPublicKey", &KeyGeneration::getPublicKey);
 }
-
-//int main(int argc, const char * argv[]) {
- //   Key testKey;
-//    testKey.initialize("4b8e29b9b0dddd58a709edba7d6df6c07ebdaf5653e325114bc5318c238f87f0");
-//    cout << "Private key is: "<<testKey.getPrivateKey()<<endl;
-//    cout << "Public key is: "<<testKey.getPublicKey()<<endl;
-//    return 0;
-//}
